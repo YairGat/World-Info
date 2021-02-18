20292b6e194050d06f03f244e6b5d17b1c086db4
YairGat <77198582+YairGat@users.noreply.github.com>
2021-02-18 19:07:24 +0200

    Delete Information.py

--- Information.py
+++
@@ -1,5 +0,0 @@
-class Information:
-    def __init__(self):
-        self.capitals = {'Asia': ['Jerusalem'], 'Europe': ['Madrid', 'Rome']}
-        self.information_type = ['Time', 'Weather', 'Local News']
-
